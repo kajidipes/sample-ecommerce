55a1af94aaef267947e1e1d71b7ed1e8eeb0fc53
ishabaral <baralisha.16@gmail.com>
2020-10-19 22:12:11 +0545

    final modification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <Footer/>
     </div>
   );
 }
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import DisplayProduct from './DisplayProduct'
 import SearchTab from './SearchTab'
 import { Container} from 'react-bootstrap'
+import DisplayProduct from './DisplayProduct'
 
 function HomePage() {
     return (
         <div>
             <Container className="mb-5">
-            <SearchTab/>
-            <DisplayProduct/>
+                <SearchTab/>
+                <DisplayProduct/>
             </Container>
         </div>
     )
