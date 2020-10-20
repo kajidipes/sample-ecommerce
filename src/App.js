551b5828a658c4f77b6059f9df772034cdbe65b6
ishabaral <baralisha.16@gmail.com>
2020-10-20 17:15:18 +0545

    trying to use router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
+import ProductDetailPage from './components/ProductDetailPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    {/* <Router>
+      <Route path = '../components/ProductDetailPage' component= {ProductDetailPage}/>   */}
       <HomePage/>
+      <ProductDetailPage />
       <Footer/>
+    {/* </Router> */}
     </div>
   );
 }
