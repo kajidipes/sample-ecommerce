fa107e479ca63b4d00079ed916c5ab1ff98b7480
kajidipes <dipesge@gmail.com>
2020-10-20 22:32:09 +0545

    Update style.js to change card hover to image only
    Check css

--- src/components/HomePage/DisplayProduct/style.js
+++ src/components/HomePage/DisplayProduct/style.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const DisplayWrapper = styled.div`
     .zoom {
-        transition: transform .3s;        
-    }
+    transition: transform 0.3s;
+  }
 
-    .zoom:hover {
-        -ms-transform: scale(1.1); /* IE 9 */
-        -webkit-transform: scale(1.1); /* Safari 3-8 */
-        transform: scale(1.1); 
-    }
+  .zoom img {
+    padding: 25px;
+  }
+  .zoom:hover img {
+    -ms-transform: scale(1.1); /* IE 9 */
+    -webkit-transform: scale(1.1); /* Safari 3-8 */
+    transform: scale(1.1);
+  }
 `;
